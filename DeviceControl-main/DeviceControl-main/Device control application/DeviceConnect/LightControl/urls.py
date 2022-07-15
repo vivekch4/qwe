e3f4aab69bb74c0bf529963ca7d2bbe33bc5d784
vivekch4 <99813364+vivekch4@users.noreply.github.com>
2022-07-15 16:40:00 +0530

    Add files via upload

---
+++ DeviceControl-main/DeviceControl-main/Device control application/DeviceConnect/LightControl/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index),
+    path('individualLight/', views.individualLight),
+    path('device_Connect/', views.device_Connect),
+    path('show_LightList/', views.showLightList),
+    path('ReadHistory/', views.readHistory),
+    path('settings/', views.configSetting),
+    path('empty/', views.clearHistory),
+    path('export/', views.exportExecl)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
